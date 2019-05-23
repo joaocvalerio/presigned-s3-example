6ab4976cbf8b794b783eb24328828f8765549c3f
joaovalerio <joaocvalerio@gmail.com>
2019-05-23 17:37:43 +0100

    added new methods. Fixed somes bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include Pundit
+
   def json_response(object, include_data: '', custom_include_data: nil, include_params: nil, meta: {}, status: :ok)
     render_data = {
       json: object,
@@ -12,12 +14,30 @@ class ApplicationController < ActionController::API
     render(render_data)
   end
 
+  AuthorizationException = Class.new(StandardError)
+
   rescue_from StandardError do |exception|
     render_server_error!
   end
 
+  rescue_from ApplicationController::AuthorizationException do |exception|
+    render_unauthorized!
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def pundit_user
+    current_user
+  end
+
+  def user_not_authorized(exception)
+    json_response(Empty.new, status: :unauthorized,
+      meta: { status: 'KO', message: "Unauthorized #{exception.policy.class.to_s.underscore.camelize}.#{exception.query}" })
+  end
+
   def render_server_error!
-    json_response(Empty.new,
+    json_response(
+      Empty.new,
       meta: { status: 'KO', message: 'Something went wrong.' },
       status: :internal_server_error
     )
@@ -28,6 +48,22 @@ class ApplicationController < ActionController::API
       Empty.new,
       meta: { status: 'KO', message: serialize_errors(errors) },
       status: :unprocessable_entity
+    )
+  end
+
+  def render_not_found!
+    json_response(
+      Empty.new,
+      status: :not_found,
+      meta: { status: 'KO', message: 'The resource you are looking for does not exist' }
+    )
+  end
+
+  def render_unauthorized!
+    json_response(
+      Empty.new,
+      status: :unauthorized,
+      meta: { status: 'KO', message: 'Authorization failed' }
     )
   end
 
